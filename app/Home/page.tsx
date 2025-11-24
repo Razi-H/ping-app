ee983307780d5858cf5ba1f8b09ac66780afcd9d
Razi <info@razi.ws>
2025-11-24 19:05:41 +0330

    New Buttons Added TO Main View

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -3,6 +3,8 @@ import '@splidejs/react-splide/css';
 import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
 import { useEffect, useRef, useState } from 'react';
 import { Sheet } from 'react-modal-sheet';
+import AddUser from './AddUser';
+import WaitingList from './WaitingList';
 
 
 export default function Home() {
@@ -64,7 +66,7 @@ export default function Home() {
             <SplideTrack>
                 <SplideSlide>
                     <div className="flex py-6 mx-2 ">
-                        <div className="w-full aspect-[19/9] bg-gradient-to-r from-indigo-500 to-blue-500 rounded-3xl flex flex-row-reverse">
+                        <div className="w-full aspect-[19/9] bg-gradient-to-r to-indigo-500 from-blue-500 rounded-3xl flex flex-row-reverse">
                             <div className="flex flex-col flex-1 justify-center justify-items-center">
                                 <div className="text-white font-bold text-md h-8 text-right">ظرفیت دو نفره</div>
                                 <div className="text-white text-sm text-right ">تعداد افراد در لیست انتظار : 10 نفر</div>
@@ -160,7 +162,7 @@ export default function Home() {
 
         </Splide>
         <div>
-            <div className="flex flex-row-reverse py-3">
+            <div className="flex flex-row-reverse py-3 hover:bg-gray-100"  onClick={() => { setOpen(true) }}>
                 <div className="mx-3">
                     <div className='w-12 h-12 bg-blue-500 rounded-3xl flex items-center justify-center'>
                         <svg className="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
@@ -169,7 +171,7 @@ export default function Home() {
 
                     </div>
                 </div>
-                <div className='flex flex-1 flex-col justify-between py-1' onClick={() => { setOpen(true) }}>
+                <div className='flex flex-1 flex-col justify-between py-1'>
                     <div className='text-sm text-right'>افزودن به لیست انتظار</div>
                     <div className='text-xs text-gray-400 text-right'>افزودن فرد جدید به لیست انتظار</div>
 
@@ -180,28 +182,7 @@ export default function Home() {
                     </svg>
                 </div>
             </div>
-            <div className="flex flex-row-reverse py-3">
-                <div className="mx-3">
-                    <div className='w-12 h-12 bg-blue-500 rounded-3xl flex items-center justify-center'>
-                        <svg className="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                            <path fillRule="evenodd" d="M12 6a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7Zm-1.5 8a4 4 0 0 0-4 4 2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-3Zm6.82-3.096a5.51 5.51 0 0 0-2.797-6.293 3.5 3.5 0 1 1 2.796 6.292ZM19.5 18h.5a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-1.1a5.503 5.503 0 0 1-.471.762A5.998 5.998 0 0 1 19.5 18ZM4 7.5a3.5 3.5 0 0 1 5.477-2.889 5.5 5.5 0 0 0-2.796 6.293A3.501 3.501 0 0 1 4 7.5ZM7.1 12H6a4 4 0 0 0-4 4 2 2 0 0 0 2 2h.5a5.998 5.998 0 0 1 3.071-5.238A5.505 5.505 0 0 1 7.1 12Z" clipRule="evenodd" />
-                        </svg>
-
-
-                    </div>
-                </div>
-                <div className='flex flex-1 flex-col justify-between py-1'>
-                    <div className='text-sm text-right'>نمایش لیست انتظار</div>
-                    <div className='text-xs text-gray-400 text-right'>نمایش افراد در لیست انتظار</div>
-
-                </div>
-                <div className="w-12 flex items-center justify-center">
-                    <svg className="w-7 h-7 text-gray-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m15 19-7-7 7-7" />
-                    </svg>
-                </div>
-            </div>
-            <div className="flex flex-row-reverse py-3">
+            <div className="flex flex-row-reverse py-3 hover:bg-gray-100 ">
                 <div className="mx-3">
                     <div className='w-12 h-12 bg-blue-500 rounded-3xl flex items-center justify-center'>
                         <svg className="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
@@ -224,7 +205,28 @@ export default function Home() {
                     </svg>
                 </div>
             </div>
-            <div className="flex flex-row-reverse py-3">
+            <div className="flex flex-row-reverse py-3 hover:bg-gray-100 ">
+                <div className="mx-3">
+                    <div className='w-12 h-12 bg-blue-500 rounded-3xl flex items-center justify-center'>
+                        <svg className="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                            <path fillRule="evenodd" d="M12 6a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7Zm-1.5 8a4 4 0 0 0-4 4 2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-3Zm6.82-3.096a5.51 5.51 0 0 0-2.797-6.293 3.5 3.5 0 1 1 2.796 6.292ZM19.5 18h.5a2 2 0 0 0 2-2 4 4 0 0 0-4-4h-1.1a5.503 5.503 0 0 1-.471.762A5.998 5.998 0 0 1 19.5 18ZM4 7.5a3.5 3.5 0 0 1 5.477-2.889 5.5 5.5 0 0 0-2.796 6.293A3.501 3.501 0 0 1 4 7.5ZM7.1 12H6a4 4 0 0 0-4 4 2 2 0 0 0 2 2h.5a5.998 5.998 0 0 1 3.071-5.238A5.505 5.505 0 0 1 7.1 12Z" clipRule="evenodd" />
+                        </svg>
+
+
+                    </div>
+                </div>
+                <div className='flex flex-1 flex-col justify-between py-1'>
+                    <div className='text-sm text-right'>نمایش لیست انتظار</div>
+                    <div className='text-xs text-gray-400 text-right'>نمایش افراد در لیست انتظار</div>
+
+                </div>
+                <div className="w-12 flex items-center justify-center">
+                    <svg className="w-7 h-7 text-gray-400 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m15 19-7-7 7-7" />
+                    </svg>
+                </div>
+            </div>
+            <div className="flex flex-row-reverse py-3 hover:bg-gray-100">
                 <div className="mx-3">
                     <div className='w-12 h-12 bg-blue-500 rounded-3xl flex items-center justify-center'>
                         <svg className="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
@@ -243,11 +245,11 @@ export default function Home() {
                     </svg>
                 </div>
             </div>
-            <div className="flex flex-row-reverse py-3">
+            <div className="flex flex-row-reverse py-3 hover:bg-gray-100 active:bg-gray-100 ">
                 <div className="mx-3">
                     <div className='w-12 h-12 bg-red-700 rounded-3xl flex items-center justify-center'>
                         <svg className="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
                         </svg>
 
                     </div>
@@ -264,72 +266,16 @@ export default function Home() {
                 </div>
             </div>
         </div>
-        <Sheet isOpen={isOpen} disableDismiss={true} initialSnap={1} detent="content" snapPoints={snapPoints} onClose={() => setOpen(false)}>
+        <Sheet isOpen={isOpen}
+        tweenConfig={{ ease: 'easeOut', duration: 0.4 }}
+        // disableDismiss={true}
+        detent='content'
+         initialSnap={1} snapPoints={snapPoints} onClose={() => setOpen(false)}>
             <Sheet.Container className='!rounded-t-2xl'>
                 <Sheet.Header />
-                <Sheet.Content><div className="flex bg-white rounded-3xl px-5 flex-col">
-                    <div className="mb-4 w-full py-2">
-                        <div className="">
-                            <label className="block mt-2 mb-2 text-sm font-medium text-gray-900 dark:text-white text-right">تلفن همراه</label>
-                            <input type="number" pattern="[0-9]*" inputMode="numeric" name="name" id="name" className="text-right bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="تلفن همراه را وارد نمایید" />
-                        </div>
-                        <div className="mt-4 mx-1 flex flex-row w-full " style={{ direction: "rtl" }}>
-                            <div className="flex items-center flex-1">
-                                <input id="country-option-1" type="radio" name="countries" value="USA" className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" defaultChecked={true} />
-                                <label className="block ms-2 px-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                                    جناب آقای
-                                </label>
-                            </div>
-                            <div className="flex items-center flex-1">
-                                <input id="country-option-1" type="radio" name="countries" value="USA" className="w-4 h-4 border-gray-300 focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 dark:focus:bg-blue-600 dark:bg-gray-700 dark:border-gray-600" />
-                                <label className="block ms-2 px-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                                    سرکار خانم
-                                </label>
-                            </div>
-                        </div>
-                        <div className="mt-4">
-                            {/* <label className="block mt-2 mb-2 text-sm font-medium text-gray-900 dark:text-white text-right">نام خانوادگی</label> */}
-                            <input type="text" name="name" id="name" className="text-right bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="نام خانوادگی را وارد نمایید" />
-                        </div>
-
-
-
-
-
-                        {/* <div className="relative z-0 w-full mb-5 group">
-                                    <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                                    <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
-                                </div> */}
-                        {/* <div className="w-full">
-                                <label for="brand" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Brand</label>
-                                <input type="text" name="brand" id="brand" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="Apple" placeholder="Product brand" required="">
-                            </div>
-                            <div className="w-full">
-                                <label for="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
-                                <input type="number" name="price" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="2999" placeholder="$299" required="">
-                            </div>
-                            <div>
-                                <label for="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
-                                <select id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                    <option selected="">Electronics</option>
-                                    <option value="TV">TV/Monitors</option>
-                                    <option value="PC">PC</option>
-                                    <option value="GA">Gaming/Console</option>
-                                    <option value="PH">Phones</option>
-                                </select>
-                            </div>
-                            <div>
-                                <label for="item-weight" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Item Weight (kg)</label>
-                                <input type="number" name="item-weight" id="item-weight" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="15" placeholder="Ex. 12" required="">
-                            </div>
-                            <div className="sm:col-span-2">
-                                <label for="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                                <textarea id="description" rows="8" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Write a product description here...">Standard glass, 3.8GHz 8-core 10th-generation Intel Core i7 processor, Turbo Boost up to 5.0GHz, 16GB 2666MHz DDR4 memory, Radeon Pro 5500 XT with 8GB of GDDR6 memory, 256GB SSD storage, Gigabit Ethernet, Magic Mouse 2, Magic Keyboard - US</textarea>
-                            </div> */}
-                    </div>
-                    <div><button className="w-full mb-6 mt-10 text-white bg-blue-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-xl text-sm px-5 py-4 me-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">افزودن به لیست انتظار</button></div>
-
-                </div>
+                <Sheet.Content>
+                    <AddUser />
+                    <WaitingList />
                 </Sheet.Content>
             </Sheet.Container>
             <Sheet.Backdrop className='!pointer-events-auto' onTap={() => setOpen(false)} />
@@ -351,7 +297,7 @@ export default function Home() {
                 </button>
                 <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
                     <svg className="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2" />
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2" />
                     </svg>
                     <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Settings</span>
                 </button>
