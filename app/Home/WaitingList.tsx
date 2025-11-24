0fdf04ba2858e80819958c80f9a3748f8eb219c6
Razi <info@razi.ws>
2025-11-24 19:03:46 +0330

    WaitingList Added

---
+++ app/Home/WaitingList.tsx
@@ -0,0 +1,43 @@
+export default function WaitingList() {
+    return (
+        <div className='w-full flex-col pb-10'>
+            <div className='even:bg-gray-50 w-full flex flex-row-reverse py-3 text-sm'>
+                <div className='text-center px-4'>1</div>
+                <div className='text-right flex-1'>آقای حسینخانی</div>
+                {/* <div className='text-right'>09123840815</div> */}
+                <div className='text-left direction-rtl flex-1 text-xs'>2 دقیقه پیش</div>
+                <div className='pl-4 text-left flex-1'><span className="bg-purple-100 text-purple-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-gray-700 dark:text-purple-400 border border-purple-400">در حال تماس</span></div>
+            </div>
+            <div className='even:bg-gray-50 w-full flex flex-row-reverse justify-around py-3 text-sm'>
+                <div className='text-center px-4'>2</div>
+                <div className='text-right flex-1'>آقای حسینخانی</div>
+                {/* <div className='text-right'>09123840815</div> */}
+                <div className='text-left direction-rtl flex-1 text-xs'>2 دقیقه پیش</div>
+                <div className='pl-4 text-left flex-1'><span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-gray-700 dark:text-blue-400 border border-blue-400">در انتظار</span></div>
+            </div>
+            <div className='even:bg-gray-50 w-full flex flex-row-reverse justify-around py-3 text-sm'>
+                <div className='text-center px-4'>3</div>
+                <div className='text-right flex-1'>آقای حسینخانی</div>
+                {/* <div className='text-right'>09123840815</div> */}
+                <div className='text-left direction-rtl flex-1 text-xs'>2 دقیقه پیش</div>
+                <div className='pl-4 text-left flex-1'><span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-gray-700 dark:text-blue-400 border border-blue-400">در انتظار</span></div>
+            </div>
+            <div className='even:bg-gray-50 w-full flex flex-row-reverse justify-around py-3 text-sm'>
+                <div className='text-center px-4'>4</div>
+                <div className='text-right flex-1'>آقای حسینخانی</div>
+                {/* <div className='text-right'>09123840815</div> */}
+                <div className='text-left direction-rtl flex-1 text-xs'>2 دقیقه پیش</div>
+                <div className='pl-4 text-left flex-1'><span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-gray-700 dark:text-blue-400 border border-blue-400">در انتظار</span></div>
+            </div>
+            <div className='even:bg-gray-50 w-full flex flex-row-reverse justify-around py-3 text-sm'>
+                <div className='text-center px-4'>5</div>
+                <div className='text-right flex-1'>آقای حسینخانی</div>
+                {/* <div className='text-right'>09123840815</div> */}
+                <div className='text-left direction-rtl flex-1 text-xs'>2 دقیقه پیش</div>
+                <div className='pl-4 text-left flex-1'><span className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-gray-700 dark:text-green-400 border border-green-400">تماس موفق</span></div>
+            </div>
+            {/* <div className="px-4" ><button className="w-full mt-6 mx-auto text-white bg-blue-500 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-xl text-sm py-3 me-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">بازگشت</button></div> */}
+
+        </div>
+    )
+}
